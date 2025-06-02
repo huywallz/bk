d6ac3a3fa356146cd4bbe3f27271241b159a3799
huywallz <136557193+huywallz@users.noreply.github.com>
2025-06-03 02:40:53 +0700

    Update bk_math.h

--- bk_math.h
+++ bk_math.h
@@ -1,5 +1,5 @@
 /*
-bk_math.h - Math utilities for the Brickate project
+bkm_math.h - Math utilities for the Brickate project
 
 This header provides basic vector (vec3) and matrix (mat4) operations
 using a column-major layout compatible with OpenGL.
@@ -15,101 +15,101 @@ Includes functions for:
 Designed for minimal dependencies and efficient use in 3D applications.
 */
 
-#ifndef BK_MATH_H
-#define BK_MATH_H
+#ifndef bkm_MATH_H
+#define bkm_MATH_H
 
 #include <math.h>
 
-float bk_math_deg(float x) {
+float bkm_deg(float x) {
 	return x * (180.0f / M_PI);
 }
 
-float bk_math_rad(float x) {
+float bkm_rad(float x) {
 	return x * (M_PI / 180.0f);
 }
 
-float bk_math_clamp(float x, float min, float max) {
+float bkm_clamp(float x, float min, float max) {
 	return x < min ? min : (x > max ? max : x);
 }
 
 typedef float vec3[3];
 
-static const vec3 BK_VEC3_ZERO = {0.0f, 0.0f, 0.0f};
+static const vec3 bkm_VEC3_ZERO = {0.0f, 0.0f, 0.0f};
 
-void bk_vec3_add(vec3 a, vec3 b, vec3 dest) {
+void bkm_add(vec3 a, vec3 b, vec3 dest) {
 	dest[0] = a[0] + b[0];
 	dest[1] = a[1] + b[1];
 	dest[2] = a[2] + b[2];
 }
 
-void bk_vec3_sub(vec3 a, vec3 b, vec3 dest) {
+void bkm_vec3_sub(vec3 a, vec3 b, vec3 dest) {
 	dest[0] = a[0] - b[0];
 	dest[1] = a[1] - b[1];
 	dest[2] = a[2] - b[2];
 }
 
-void bk_vec3_scale(vec3 a, float s, vec3 dest) {
+void bkm_vec3_scale(vec3 a, float s, vec3 dest) {
 	dest[0] = a[0] * s;
 	dest[1] = a[1] * s;
 	dest[2] = a[2] * s;
 }
 
-float bk_vec3_dot(vec3 a, vec3 b) {
+float bkm_vec3_dot(vec3 a, vec3 b) {
 	return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
 }
 
-void bk_vec3_cross(vec3 a, vec3 b, vec3 dest) {
+void bkm_vec3_cross(vec3 a, vec3 b, vec3 dest) {
 	dest[0] = a[1] * b[2] - a[2] * b[1];
 	dest[1] = a[2] * b[0] - a[0] * b[2];
 	dest[2] = a[0] * b[1] - a[1] * b[0];
 }
 
-float bk_vec3_len(vec3 v) {
+float bkm_vec3_len(vec3 v) {
 	return sqrtf(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
 }
 
-void bk_vec3_copy(vec3 v, vec3 dest) {
+void bkm_vec3_copy(vec3 v, vec3 dest) {
 	dest[0] = v[0];
 	dest[1] = v[1];
 	dest[2] = v[2];
 }
 
-void bk_vec3_set(vec3 dest, float x, float y, float z) {
+void bkm_vec3_set(vec3 dest, float x, float y, float z) {
 	dest[0] = x;
 	dest[1] = y;
 	dest[2] = z;
 }
 
-void bk_vec3_normalize(vec3 v, vec3 dest) {
-	float len = bk_vec3_len(v);
+void bkm_vec3_normalize(vec3 v, vec3 dest) {
+	float len = bkm_vec3_len(v);
 	if (len > 0.0f)
-		bk_vec3_scale(v, 1.0f / len, dest);
-	else bk_vec3_copy(BK_VEC3_ZERO, dest);
+		bkm_vec3_scale(v, 1.0f / len, dest);
+	else bkm_vec3_copy(bkm_VEC3_ZERO, dest);
 }
 
 typedef float mat4[16];
 
-void bk_mat4_identity(mat4 dest) {
+void bkm_mat4_identity(mat4 dest) {
 	for (int i = 0; i < 16; i++) dest[i] = 0.0f;
 	dest[0] = dest[5] = dest[10] = dest[15] = 1.0f;
 }
 
-void bk_mat4_translate(vec3 v, mat4 dest) {
-	bk_mat4_identity(m);
+void bkm_mat4_translate(vec3 v, mat4 dest) {
+	bkm_mat4_identity(m);
 	dest[12] = v[0];
 	dest[13] = v[1];
 	dest[14] = v[2];
 }
 
-void bk_mat4_scale(vec3 v, mat4 dest) {
-	bk_mat4_identity(m);
+void bkm_mat4_scale(vec3 v, mat4 dest) {
+	bkm_mat4_identity(m);
 	dest[0] = v[0];
 	dest[5] = v[1];
 	dest[10] = v[2];
 }
 
-void bk_mat4_rotate_x(float angle_rad, mat4 dest) {
-	bk_mat4_identity(m);
+void bkm_mat4_rotate_x(float angle_rad, mat4 dest) {
+	bkm_mat4_identity(m);
 	float s = sinf(angle_rad);
 	float c = cosf(angle_rad);
 	dest[5] = c;
@@ -118,8 +118,8 @@ void bk_mat4_rotate_x(float angle_rad, mat4 dest) {
 	dest[10] = c;
 }
 
-void bk_mat4_rotate_y(float angle_rad, mat4 dest) {
-	bk_mat4_identity(m);
+void bkm_mat4_rotate_y(float angle_rad, mat4 dest) {
+	bkm_mat4_identity(m);
 	float s = sinf(angle_rad);
 	float c = cosf(angle_rad);
 	dest[0] = c;
@@ -128,8 +128,8 @@ void bk_mat4_rotate_y(float angle_rad, mat4 dest) {
 	dest[10] = c;
 }
 
-void bk_mat4_rotate_z(float angle_rad, mat4 dest) {
-	bk_mat4_identity(m);
+void bkm_mat4_rotate_z(float angle_rad, mat4 dest) {
+	bkm_mat4_identity(m);
 	float s = sinf(angle_rad);
 	float c = cosf(angle_rad);
 	dest[0] = c;
@@ -138,7 +138,7 @@ void bk_mat4_rotate_z(float angle_rad, mat4 dest) {
 	dest[5] = c;
 }
 
-void bk_mat4_mul(mat4 a, mat4 b, mat4 dest) {
+void bkm_mat4_mul(mat4 a, mat4 b, mat4 dest) {
 	mat4 res;
 	for (int col = 0; col < 4; col++) {
 		for (int row = 0; row < 4; row++) {
@@ -177,17 +177,17 @@ void mat4_perspective(float fovy, float aspect, float near, float far, mat4 dest
 	dest[15] = 0.0f;
 }
 
-void bk_mat4_lookat(vec3 eye, vec3 center, vec3 up, mat4 dest) {
+void bkm_mat4_lookat(vec3 eye, vec3 center, vec3 up, mat4 dest) {
 	vec3 f, s, u;
 	
-	bk_vec3_sub(center, eye, f);
-	bk_vec3_normalize(f, f);
+	bkm_vec3_sub(center, eye, f);
+	bkm_vec3_normalize(f, f);
 	
-	bk_vec3_cross(f, up, s);
-	bk_vec3_normalize(s, s);
+	bkm_vec3_cross(f, up, s);
+	bkm_vec3_normalize(s, s);
 	
-	bk_vec3_cross(s, f, u);
-	bk_vec3_normalize(u, u);
+	bkm_vec3_cross(s, f, u);
+	bkm_vec3_normalize(u, u);
 	
 	dest[0] = s[0];
 	dest[1] = u[0];
@@ -204,32 +204,32 @@ void bk_mat4_lookat(vec3 eye, vec3 center, vec3 up, mat4 dest) {
 	dest[10] = -f[2];
 	dest[11] = 0.0f;
 	
-	dest[12] = -bk_vec3_dot(s, eye);
-	dest[13] = -bk_vec3_dot(u, eye);
-	dest[14] = bk_vec3_dot(f, eye);
+	dest[12] = -bkm_vec3_dot(s, eye);
+	dest[13] = -bkm_vec3_dot(u, eye);
+	dest[14] = bkm_vec3_dot(f, eye);
 	dest[15] = 1.0f;
 }
 
-void bk_mat4_model(vec3 pos, vec3 rot, vec3 scale, mat4 dest) {
+void bkm_mat4_model(vec3 pos, vec3 rot, vec3 scale, mat4 dest) {
 	mat4 t, rx, ry, rz, s, rxy, rxyz, trs;
 	
-	bk_mat4_translate(pos, t);
+	bkm_mat4_translate(pos, t);
 	
-	bk_mat4_rotate_x(rot[0], rx);
-	bk_mat4_rotate_y(rot[1], ry);
-	bk_mat4_rotate_z(rot[2], rz);
+	bkm_mat4_rotate_x(rot[0], rx);
+	bkm_mat4_rotate_y(rot[1], ry);
+	bkm_mat4_rotate_z(rot[2], rz);
 	
-	bk_mat4_scale(scale, s);
+	bkm_mat4_scale(scale, s);
 	
-	bk_mat4_mul(ry, rx, rxy);
-	bk_mat4_mul(rz, rxy, rxyz);
+	bkm_mat4_mul(ry, rx, rxy);
+	bkm_mat4_mul(rz, rxy, rxyz);
 	
-	bk_mat4_mul(rxyz, s, trs);
+	bkm_mat4_mul(rxyz, s, trs);
 	
-	bk_mat4_mul(t, trs, dest);
+	bkm_mat4_mul(t, trs, dest);
 }
 
-void bk_mat4_mulv(mat4 m, vec3 v, float w, mat4 dest) {
+void bkm_mat4_mulv(mat4 m, vec3 v, float w, mat4 dest) {
 	dest[0] = m[0] * v[0] + m[4] * v[1] + m[8]  * v[2] + m[12] * w;
 	dest[1] = m[1] * v[0] + m[5] * v[1] + m[9]  * v[2] + m[13] * w;
 	dest[2] = m[2] * v[0] + m[6] * v[1] + m[10] * v[2] + m[14] * w;
